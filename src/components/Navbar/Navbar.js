5011a5f3a3ff5d43a85c84e1332d7af80c572b16
timstarkk <timstarkk@icloud.com>
2020-03-14 00:46:00 -0500

    initial creation of navbar rcc with css import

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Nabar.css';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                hello from navbar
+            </div>
+        )
+    }
+}
