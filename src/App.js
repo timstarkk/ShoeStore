508146151e33d47e983847fefd5d7a0345a03741
timstarkk <timstarkk@icloud.com>
2020-03-14 14:37:16 -0500

    import/call navbar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
