9d804f2adb7ef93f7523c3c6c8eaa69feb4f6b41
timstarkk <timstarkk@icloud.com>
2020-03-14 00:42:52 -0500

    imported Home component from /pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div>hello from app</div>
