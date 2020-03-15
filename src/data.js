cf4bc26fdab4b2032a97aae0220717583156d28e
timstarkk <timstarkk@icloud.com>
2020-03-15 00:19:01 -0500

    created file and imported initial fake data

---
+++ src/data.js
@@ -0,0 +1,115 @@
+import img1 from './images/placeholder480.jpg';
+import img2 from './images/placeholder190.jpg'
+
+export default [
+    {
+        sys: {
+            id: '1'
+        },
+        fields: {
+            name: 'item 1',
+            slug: 'item-1',
+            type: 'custom',
+            price: 100,
+            featured: true,
+            description: 'an amazing item from the world of items',
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: img1
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        sys: {
+            id: '2'
+        },
+        fields: {
+            name: 'item 2',
+            slug: 'item-2',
+            type: 'custom',
+            price: 200,
+            featured: true,
+            description: 'an amazing item from the world of items',
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: img2
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        sys: {
+            id: '3'
+        },
+        fields: {
+            name: 'item 3',
+            slug: 'item-3',
+            type: 'custom',
+            price: 300,
+            featured: true,
+            description: 'an amazing item from the world of items',
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: img1
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        sys: {
+            id: '4'
+        },
+        fields: {
+            name: 'item 4',
+            slug: 'item-4',
+            type: 'custom',
+            price: 400,
+            featured: true,
+            description: 'an amazing item from the world of items',
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: img2
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        sys: {
+            id: '5'
+        },
+        fields: {
+            name: 'item 5',
+            slug: 'item-5',
+            type: 'custom',
+            price: 500,
+            featured: false,
+            description: 'an amazing item from the world of items',
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: img1
+                        }
+                    }
+                }
+            ]
+        }
+    }
+]
