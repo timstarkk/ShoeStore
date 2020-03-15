83883adfef8d821178e0ebc576584bdc5d954a6e
timstarkk <timstarkk@icloud.com>
2020-03-15 00:19:27 -0500

    created Context with DidMount and formatData

--- src/context.js
+++ src/context.js
@@ -3,12 +3,69 @@ import items from './data';
 
 const ShopContext = React.createContext();
 
-export default class ShopProvider extends Component {
+class ShopProvider extends Component {
+    state = {
+        shopItems: [],
+        sortedItems: [],
+        featuredItems: [],
+        loading: true,
+        type: 'all',
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+    };
+
+    componentDidMount() {
+        let shopItems = this.formatData(items);
+        console.log(shopItems);
+        let featuredItems = shopItems.filter(item => item.featured === true)
+        let maxPrice = Math.max(...shopItems.map(item => item.price));
+        this.setState({
+            shopItems,
+            featuredItems,
+            sortedItems: shopItems,
+            loading: false,
+            price: maxPrice,
+            maxPrice
+        })
+    };
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(images => images.fields.file.url);
+            let item = { ...item.fields, images, id }
+
+            return item
+        });
+
+        return tempItems;
+    };
+
+
+
     render() {
         return (
-            <div>
-
-            </div>
+            <RoomContext.Provider value={{
+                ...this.state,
+                // getRoom: this.getRoom,
+                // handleChange: this.handlechange
+            }}>
+                {this.props.children}
+            </RoomContext.Provider>
         )
-    }
+    };
 }
+
+
+const ShopConsumer = ShopContext.Consumer;
+
+export function withShopConsumer(Component) {
+    return function ConsumerWrapper(props) {
+        return <ShopConsumer>
+            {value => <Component {...props} context={value} />}
+        </ShopConsumer>
+    }
+};
+
+export { ShopProvider, ShopConsumer, ShopContext }
