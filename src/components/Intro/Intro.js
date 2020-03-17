f5bb6ddf33c155eb74d1bf1bdd1adb3e889e1290
timstarkk <timstarkk@icloud.com>
2020-03-17 00:19:49 -0500

    built out intro section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Intro.css';
+import Title from '../Title/Title';
+import Image from '../../images/placeholder480.jpg';
 
 export default function Intro() {
     return (
-        <div>
-            hello from introduction section
-        </div>
+        <section className="intro-section">
+            <Title title="what you really want to wear" />
+            <div className="intro-text-container">
+                <p>
+                    Don't wait for the crowd. Get the sneakers that fit your style. We have the latest and greatest pieces for you to add to your collection and show yourself off.
+                </p>
+            </div>
+            <div className="intro-feature">
+                <img src={Image} alt="dsfasd" />
+                <div className="intro-feature-text">
+                    <h4>hello from in here</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ex inventore, sint asperiores sequi accusantium soluta consequatur reiciendis illo ad incidunt, perspiciatis modi iure architecto voluptas, quidem doloremque vitae porro?</p>
+                    <div className="btn-container">
+                        <div className="btn btn-primary">get some</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
