688acfd300fcfef60000df2761c31a24c0088bf7
timstarkk <timstarkk@icloud.com>
2020-03-17 04:53:33 -0500

    added link to button

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Intro.css';
 import Title from '../Title/Title';
 import Image from '../../images/sneakerImage.jpeg';
@@ -18,7 +19,9 @@ export default function Intro() {
                     <h4>only the best</h4>
                     <p>Our selection is handpicked from across the world by a seasoned team of individuals who have a proven taste for the highest quality footwear. We look and find the hidden gems, so you don't have to.</p>
                     <div className="btn-container">
-                        <div className="btn btn-primary">get some</div>
+                        <Link to="/store">
+                            <div className="btn btn-primary">get some</div>
+                        </Link>
                     </div>
                 </div>
             </div>
