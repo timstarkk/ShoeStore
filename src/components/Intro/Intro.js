188c2d897539c861b3e2bdc7e7b7b0448c24d03d
timstarkk <timstarkk@icloud.com>
2020-03-16 22:49:52 -0500

    initial creation of Intro rfc

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Intro.css';
+
+export default function Intro() {
+    return (
+        <div>
+            hello from introduction section
+        </div>
+    )
+}
