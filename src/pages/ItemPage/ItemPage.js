317ed5efcda12184ba7a71659600b3b8a7601976
timstarkk <timstarkk@icloud.com>
2020-03-18 02:56:39 -0500

    moved ItemPage.js to folder

--- src/pages/ItemPage/ItemPage.js
+++ src/pages/ItemPage/ItemPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ItemContext } from '../context';
+import { ItemContext } from '../../context';
 import { Link } from 'react-router-dom';
 
 export default class ItemPage extends Component {
