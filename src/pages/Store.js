2deee8c7eed5ddcad20af0f38a0e8f24e03b9cd7
timstarkk <timstarkk@icloud.com>
2020-03-18 00:04:04 -0500

    added footer

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Products from '../components/Products/Products';
+import Footer from '../components/Footer/Footer';
 
 export default function Store() {
     return (
         <>
             <Products />
+            <Footer />
         </>
     )
 }
