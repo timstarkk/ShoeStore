c139ce4772f4788b50b77bdc9ca7f4d2630c8bd3
timstarkk <timstarkk@icloud.com>
2020-03-17 23:37:54 -0500

    changed to rcc and import context

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ItemContext } from '../../context';
 import './Products.css';
 
-export default function Products() {
-    return (
-        <section className="products-section">
-            <h4>products</h4>
-            <div className='line'></div>
-            <div>hello from second div</div>
-        </section>
-    )
+export default class Products extends Component {
+    static contextType = ItemContext;
+    render() {
+        return (
+            <section className="products-section">
+                <h4>products</h4>
+                <div className='line'></div>
+                <div>hello from second div</div>
+            </section>
+        )
+    }
 }
