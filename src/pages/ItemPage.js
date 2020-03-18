0d427e5edcf0fd4f3e6d18630f730c012806a363
timstarkk <timstarkk@icloud.com>
2020-03-18 02:54:49 -0500

    imported Class component

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { ItemContext } from '../context';
+import { Link } from 'react-router-dom';
 
-export default function SingleItem() {
-    return (
-        <div>
-            hello from ItemPage
-        </div>
-    )
+export default class ItemPage extends Component {
+    static contextType = ItemContext;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            slug: this.props.match.params.slug
+        }
+    }
+
+    render() {
+        const { getItem } = this.context;
+        const item = getItem(this.state.slug);
+
+        if (!item) {
+            return <div className="error">
+                <h3>item not found.</h3>
+                <Link to='/store' className='btn btn-primary'>
+                    back to store
+                </Link>
+            </div>
+        }
+
+        return (
+            <div>
+                hello from item page
+            </div>
+        )
+    }
 }
