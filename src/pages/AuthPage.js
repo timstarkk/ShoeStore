c535dad2b3064f7c6f25f42b13b31706ae058e29
timstarkk <timstarkk@icloud.com>
2020-03-29 13:47:21 -0500

    changed Auth.js to AuthPage.js

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import SignUpForm from '../components/Auth/SignUpForm';
 
-export default function Auth() {
+export default function AuthPage() {
     return (
         <>
             <SignUpForm />
