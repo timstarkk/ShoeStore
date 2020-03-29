327292e4ca1e0e9f3a3d6f908e6ebe7c27422c05
timstarkk <timstarkk@icloud.com>
2020-03-29 02:41:08 -0500

    initial creation of auth page with footer/signup

---
+++ src/pages/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../components/Footer/Footer';
+import SignUpForm from '../components/Auth/SignUpForm';
+
+export default function Auth() {
+    return (
+        <>
+            <SignUpForm />
+            <Footer />
+        </>
+    )
+}
