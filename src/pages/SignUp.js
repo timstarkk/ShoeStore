b850f9595094dcbfd0549fbc290b6295780db5a0
timstarkk <timstarkk@icloud.com>
2020-03-30 03:00:42 -0500

    changed name to SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import SignUpForm from '../components/Account/SignUpForm';
 
-export default function Account() {
+export default function SignUp() {
     return (
         <>
             <SignUpForm />
