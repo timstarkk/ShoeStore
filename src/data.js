58fa0d73b84986f973ac95652fbebb343741bdbf
timstarkk <timstarkk@icloud.com>
2020-04-02 20:28:10 -0500

    edited to export a query only (data online now)

--- src/data.js
+++ src/data.js
@@ -1,262 +1,271 @@
-// import img1 from './images/placeholder480.jpg';
-// import img2 from './images/placeholder190.jpg';
-
-// import sneakerImage from './images/sneakerImage.jpeg';
-
-import sneaker1 from './images/sneaker1.jpg';
-import sneaker2 from './images/sneaker2.jpg';
-import sneaker3 from './images/sneaker3.jpg';
-import sneaker4 from './images/sneaker4.jpg';
-import sneaker5 from './images/sneaker5.jpg';
-import sneaker6 from './images/sneaker6.jpg';
-import sneaker7 from './images/sneaker7.jpg';
-import sneaker8 from './images/sneaker8.jpg';
-import sneaker9 from './images/sneaker9.jpg';
-import sneaker10 from './images/sneaker10.jpg';
-import sneaker11 from './images/sneaker11.jpg';
-// import sneaker12 from './images/sneaker12.jpg';
-
-export default [
-    {
-        sys: {
-            id: '1'
-        },
-        fields: {
-            name: 'item 1',
-            slug: 'item-1',
-            type: 'custom',
-            price: 100,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker1
-                        }
-                    }
-                }
-            ]
+const query = `
+query {
+    listStoreItems(limit: 999) {
+    items {
+        id
+        fields {
+        name 
+        slug
+        itemType
+        price
+        featured
+        description
+        images {
+            imageFields {
+            file {
+                url
+            }
+            }
         }
-    },
-    {
-        sys: {
-            id: '2'
-        },
-        fields: {
-            name: 'item 2',
-            slug: 'item-2',
-            type: 'custom',
-            price: 200,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker2
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '3'
-        },
-        fields: {
-            name: 'item 3',
-            slug: 'item-3',
-            type: 'custom',
-            price: 300,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker3
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '4'
-        },
-        fields: {
-            name: 'item 4',
-            slug: 'item-4',
-            type: 'custom',
-            price: 400,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker4
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '5'
-        },
-        fields: {
-            name: 'item 5',
-            slug: 'item-5',
-            type: 'custom',
-            price: 500,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker5
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '6'
-        },
-        fields: {
-            name: 'item 6',
-            slug: 'item-6',
-            type: 'custom',
-            price: 600,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker6
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '7'
-        },
-        fields: {
-            name: 'item 7',
-            slug: 'item-7',
-            type: 'custom',
-            price: 700,
-            featured: true,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker7
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '8'
-        },
-        fields: {
-            name: 'item 8',
-            slug: 'item-8',
-            type: 'custom',
-            price: 800,
-            featured: false,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker8
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '9'
-        },
-        fields: {
-            name: 'item 9',
-            slug: 'item-9',
-            type: 'custom',
-            price: 900,
-            featured: false,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker9
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '10'
-        },
-        fields: {
-            name: 'item 10',
-            slug: 'item-10',
-            type: 'custom',
-            price: 1000,
-            featured: false,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker10
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        sys: {
-            id: '11'
-        },
-        fields: {
-            name: 'item 11',
-            slug: 'item-11',
-            type: 'custom',
-            price: 1100,
-            featured: false,
-            description: 'an amazing item from the world of items',
-            images: [
-                {
-                    fields: {
-                        file: {
-                            url: sneaker11
-                        }
-                    }
-                }
-            ]
         }
     }
-]
+    }
+}
+`
+
+export default query;
+
+// const items = [
+//     {
+//         sys: {
+//             id: '1'
+//         },
+//         fields: {
+//             name: 'item 1',
+//             slug: 'item-1',
+//             type: 'custom',
+//             price: 100,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker1
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '2'
+//         },
+//         fields: {
+//             name: 'item 2',
+//             slug: 'item-2',
+//             type: 'custom',
+//             price: 200,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker2
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '3'
+//         },
+//         fields: {
+//             name: 'item 3',
+//             slug: 'item-3',
+//             type: 'custom',
+//             price: 300,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker3
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '4'
+//         },
+//         fields: {
+//             name: 'item 4',
+//             slug: 'item-4',
+//             type: 'custom',
+//             price: 400,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker4
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '5'
+//         },
+//         fields: {
+//             name: 'item 5',
+//             slug: 'item-5',
+//             type: 'custom',
+//             price: 500,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker5
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '6'
+//         },
+//         fields: {
+//             name: 'item 6',
+//             slug: 'item-6',
+//             type: 'custom',
+//             price: 600,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker6
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '7'
+//         },
+//         fields: {
+//             name: 'item 7',
+//             slug: 'item-7',
+//             type: 'custom',
+//             price: 700,
+//             featured: true,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker7
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '8'
+//         },
+//         fields: {
+//             name: 'item 8',
+//             slug: 'item-8',
+//             type: 'custom',
+//             price: 800,
+//             featured: false,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker8
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '9'
+//         },
+//         fields: {
+//             name: 'item 9',
+//             slug: 'item-9',
+//             type: 'custom',
+//             price: 900,
+//             featured: false,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker9
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '10'
+//         },
+//         fields: {
+//             name: 'item 10',
+//             slug: 'item-10',
+//             type: 'custom',
+//             price: 1000,
+//             featured: false,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker10
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     },
+//     {
+//         sys: {
+//             id: '11'
+//         },
+//         fields: {
+//             name: 'item 11',
+//             slug: 'item-11',
+//             type: 'custom',
+//             price: 1100,
+//             featured: false,
+//             description: 'an amazing item from the world of items',
+//             images: [
+//                 {
+//                     fields: {
+//                         file: {
+//                             url: sneaker11
+//                         }
+//                     }
+//                 }
+//             ]
+//         }
+//     }
+// ]
