52b919f104b2421eaf6b46ff64d1a5f720d60f25
Tim Stark <timstarkk@icloud.com>
2020-04-25 17:09:45 -0500

    added h4 and line

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,8 @@ import './About.css';
 export default function About() {
     return (
         <div className="about-section">
+            <h4>About This Site</h4>
+            <div className='line'></div> 
             hello from about page
         </div>
     )
