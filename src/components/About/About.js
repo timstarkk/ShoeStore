438318394d2e5896ea80c614c530eba93a8a28ea
Tim Stark <timstarkk@icloud.com>
2020-04-25 17:13:47 -0500

    added .container-wrapper

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,9 @@ export default function About() {
         <div className="about-section">
             <h4>About This Site</h4>
             <div className='line'></div> 
-            hello from about page
+            <div className="container-wrapper">
+                <p>This website is not intended to sell any products. For this reason, there is currently no functionality in the checkout button.</p>
+            </div>
         </div>
     )
 }
