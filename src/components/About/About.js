d92a7bdf918e9fc551cfc6091386052af170187d
Tim Stark <timstarkk@icloud.com>
2020-04-25 17:19:52 -0500

    added basic about me <p> explaining site

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,9 @@ export default function About() {
             <h4>About This Site</h4>
             <div className='line'></div> 
             <div className="container-wrapper">
-                <p>This website is not intended to sell any products. For this reason, there is currently no functionality in the checkout button.</p>
+                <p>A serverless E-commerce website built with React.js and AWS Cloud.<br />AWS services used include Amplify, Cognito, AppSync, DynamoDB, and S3.<br />
+                    The shopping cart was built without libraries, and includes cart persistence during user sign-in. <br /><br />
+                    This website is an example and is not intended to actually sell products.<br />For this reason, the checkout button currently has no functionality.</p>
             </div>
         </div>
     )
